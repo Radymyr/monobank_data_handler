7d48a330a6b6811da1d881216e5f33ee7ecc797b
Radymyr <loboda.radymyr@gmail.com>
2024-09-09 13:27:39 +0300

    updated routes

--- src/initialized.js
+++ src/initialized.js
@@ -5,5 +5,3 @@ export const webHookUrl = process.env.WEB_HOOK_URL;
 export const route = 'https://api.monobank.ua/personal/webhook';
 
 export const webhookRoute = '/monobank/webhook';
-
-export default app;
