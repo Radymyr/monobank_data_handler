1ff871b3aef94f8a927ea5f5ca255cb6ebd6c6fb
Radymyr <loboda.radymyr@gmail.com>
2024-09-15 14:32:49 +0300

    changed parse mode

--- src/utils.js
+++ src/utils.js
@@ -99,7 +99,7 @@ export async function sendToTelegram(request, reply) {
 
   if (chatId && messageBody) {
     await bot.telegram.sendMessage(chatId, monobankResponse, {
-      parse_mode: 'MarkdownV2',
+      parse_mode: 'Markdown',
     });
   }
 
